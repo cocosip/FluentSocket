bb533337a7dd3f376a2e083fef3e4d8f54410600
virtual <1185513330@qq.com>
2020-07-16 23:04:00 +0800

    Rebuild Signed-off-by: virtual <1185513330@qq.com>

---
+++ framework/src/FluentSocket/Protocols/Packet.cs
@@ -0,0 +1,11 @@
+﻿namespace FluentSocket.Protocols
+{
+    public abstract class Packet
+    {
+        public PacketType PacketType { get; set; }
+
+        public int Sequence { get; set; }
+
+        public byte[] Body { get; set; }
+    }
+}
---
+++ framework/src/FluentSocket/Protocols/PacketType.cs
@@ -0,0 +1,9 @@
+﻿namespace FluentSocket.Protocols
+{
+    public enum PacketType : byte
+    {
+        Request = 1,
+
+        Response = 2
+    }
+}
